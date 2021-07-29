5b713fe0b3d9abf5e895305b7ebcea53a3f65749
HongHyukjin <gurwlszx@naver.com>
2021-07-29 16:59:14 +0900

    HHJ

--- Greedy/Greedy_6(HHJ).py
+++ Greedy/Greedy_6(HHJ).py
@@ -6,15 +6,22 @@ k = int(input())
 
 idx = 0
 length = len(food_times)
-for i in range(0, k):
-    food_times[idx] -= 1
-    while(True):
-        idx += 1
-        if idx >= length:
-            idx %= length
-        if food_times[idx] == 0:
-            continue
-        else:
-            break
+num = 0
 
-print(idx+1)
+for i in range(0, k):
+    print(idx)
+    food_times[idx] -= 1
+    print(food_times)
+    for j in range(0, length):
+        idx = (idx + 1) % length
+        if(food_times[idx] != 0):
+            break
+        else:
+            num += 1
+            print(num)
+    if(num == length):
+        result = -1
+        break
+
+
+print(result)
