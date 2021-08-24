d0f5bd0f36d04cdf26cc4533b337dda4e02c3a75
HongHyukjin <gurwlszx@naver.com>
2021-08-24 14:33:41 +0900

    HHJ

--- DP/퇴사(HHJ).py
+++ DP/퇴사(HHJ).py
@@ -1,11 +1,17 @@
 #퇴사
+#남은 일수
 N = int(input())
+#상담 기간
 T = []
+#금액
 P = []
+#벌 수 있는 최대 금액
 result = 0
+#해당 일을 시작할 경우 다음 일을 시작할 수 있는 날
 end_day = 0
+#i일부터 마지막날까지 일했을 경우 벌 수 있는 최대 금액
 dp = [0] * N
-
+햣
 for _ in range(N):
     t, p = map(int, input().split())
     T.append(t)
