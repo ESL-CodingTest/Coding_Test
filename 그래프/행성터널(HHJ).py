6d04564c466069b6a1b2baef23503dd8e9add3ac
HongHyukjin <gurwlszx@naver.com>
2021-09-02 15:07:08 +0900

    HHJ

--- 그래프/행성터널(HHJ).py
+++ 그래프/행성터널(HHJ).py
@@ -16,8 +16,9 @@ def union_parent(parent, a, b):
 
 V = int(input())    #노드의 개수
 parent = [0] * (V+1)    #부모노드 테이블
-P = [0] * (V+1)  #노드들의 좌표
+P = [[0,0,0] * (V+1)]  #노드들의 좌표
 edges = []  #간선과 길이
+result = 0
 
 for i in range(1, V+1):
     parent[i] = i
@@ -27,16 +28,19 @@ for i in range(1, V+1):
     P.append([x,y,z])
 
 #좌표들을 가지고 간선의 길이를 구함
-for i in range(1, V):
-    for j in range(i+1, V):
+for i in range(1, V+1):
+    for j in range(i+1, V+1):
         m = min(abs(P[i][0]-P[j][0]), abs(P[i][1]-P[j][1]), abs(P[i][2]-P[j][2]))
         edges.append((m, i, j))
 
 #간선의 길이로 정렬
+print(edges)
 edges.sort()
 
 for edge in edges:
     cost, a, b = edge
-    if
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
 
-print(edges)
+print(result)
